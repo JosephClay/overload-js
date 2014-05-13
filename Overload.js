fb464181fb604ccbba59e6491f3480c17125c1e7
Joseph Clay <clay@dcim.com>
2014-05-13 14:32:12 -0500

    More noopArr

--- Overload.js
+++ Overload.js
@@ -211,7 +211,7 @@
 	};
 
 	var _getArgumentMatch = function(mappings, args) {
-		mappings = mappings || [];
+		mappings = mappings || _noopArr;
 
 		var argTypes = _convertParametersTypes(args),
 			idx = 0, length = mappings.length;
@@ -225,7 +225,7 @@
 	};
 
 	var _getLengthMatch = function(mappings, args) {
-		mappings = mappings || [];
+		mappings = mappings || _noopArr;
 
 		var argLength = args.length,
 			idx = 0, length = mappings.length;
